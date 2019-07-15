4072f2f2f2749f581cf068e66666ee2b87e19c9d
MinYoung-Lim <minyounge17@naver.com>
2019-07-15 14:00:40 +0900

    LoginActivity 만듦

---
+++ app/src/main/java/com/example/cho6/finalproject_guru2/Acitivity/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.cho6.finalproject_guru2.Acitivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cho6.finalproject_guru2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+}
