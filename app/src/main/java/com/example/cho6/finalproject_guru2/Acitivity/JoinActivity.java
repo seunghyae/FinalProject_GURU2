67b4490d09af90f553233c6f19da42602d98123a
SeonYoung-Jang <model0517@naver.com>
2019-07-15 14:08:53 +0900

    Activity, Fragment 생성

---
+++ app/src/main/java/com/example/cho6/finalproject_guru2/Acitivity/JoinActivity.java
@@ -0,0 +1,16 @@
+package com.example.cho6.finalproject_guru2.Acitivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cho6.finalproject_guru2.R;
+
+public class JoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+}
