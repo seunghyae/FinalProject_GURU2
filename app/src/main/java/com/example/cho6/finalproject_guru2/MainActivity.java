e9833ee96e8a55d33d1750a8d507f7b5334091e0
MinYoung-Lim <minyounge17@naver.com>
2019-07-15 13:36:20 +0900

    최초 업로드

---
+++ app/src/main/java/com/example/cho6/finalproject_guru2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cho6.finalproject_guru2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
