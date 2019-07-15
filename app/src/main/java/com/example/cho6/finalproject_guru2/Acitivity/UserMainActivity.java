258feb3b1936854ab8814d82115dae65bcc48b2f
SeonYoung-Jang <model0517@naver.com>
2019-07-15 15:06:44 +0900

    activity usermain

--- app/src/main/java/com/example/cho6/finalproject_guru2/Acitivity/UserMainActivity.java
+++ app/src/main/java/com/example/cho6/finalproject_guru2/Acitivity/UserMainActivity.java
@@ -1,16 +1,23 @@
 package com.example.cho6.finalproject_guru2.Acitivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.cho6.finalproject_guru2.R;
 
 public class UserMainActivity extends AppCompatActivity {
+    private ViewPager viewPager;//표시할 영역
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager=findViewById(R.id.viewPager);
+        //Viewpager생성
+
+
     }
 }
