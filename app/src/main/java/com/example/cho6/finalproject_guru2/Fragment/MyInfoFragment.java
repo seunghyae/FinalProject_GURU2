d20eb4eb5abd79a767f030c4b03a019689e77684
MinYoung-Lim <minyounge17@naver.com>
2019-07-18 10:28:53 +0900

    로그아웃 구현(오류남, 버튼누르지말기!)

--- app/src/main/java/com/example/cho6/finalproject_guru2/Fragment/MyInfoFragment.java
+++ app/src/main/java/com/example/cho6/finalproject_guru2/Fragment/MyInfoFragment.java
@@ -1,111 +1,101 @@
 package com.example.cho6.finalproject_guru2.Fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.cho6.finalproject_guru2.Acitivity.LoginActivity;
+import com.example.cho6.finalproject_guru2.Bean.MemberBean;
 import com.example.cho6.finalproject_guru2.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MyInfoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MyInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.UUID;
+
+
 public class MyInfoFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public MyInfoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MyInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MyInfoFragment newInstance(String param1, String param2) {
-        MyInfoFragment fragment = new MyInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_info, container, false);
-    }
+        View view=inflater.inflate(R.layout.fragment_my_info, container, false);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        final TextView mTxtMemId=view.findViewById(R.id.txtMemID);
+        final TextView mTxtMemName=view.findViewById(R.id.txtMemName);
+        final TextView mTxtMemMajor=view.findViewById(R.id.txtMemMajor);
+        final TextView mTxtMemNo=view.findViewById(R.id.txtMemNo);
+
+        Button btnLogout=view.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
+        //FireBase 인증객체
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        final FirebaseDatabase mFirebaseDB = FirebaseDatabase.getInstance();
+        final FirebaseUser mFirebaseUser=mFirebaseAuth.getCurrentUser();
+
+        String email;
+        email = mFirebaseUser.getEmail();
+        mTxtMemId.setText("구글ID: "+email);
+
+        mFirebaseDB.getReference().child("member").child(getUserIdFromUUID(email)).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    MemberBean bean = snapshot.getValue(MemberBean.class);
+                    String UUIDEmail = LoginActivity.getUserIdFromUUID(mFirebaseUser.getEmail());
+                    if(TextUtils.equals(bean.memId, UUIDEmail)) {
+                        mTxtMemName.setText("이름: "+bean.memName);
+                        mTxtMemMajor.setText("학과: "+bean.memMajor);
+                        mTxtMemNo.setText("학번: "+bean.memNum);
+                        break;
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        return view;
+    }
+    //로그아웃을 처리한다.
+    private void logout(){
+        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
+        try{
+            mFirebaseAuth.signOut();
+            Toast.makeText(getActivity(),"로그아웃 되었습니다.",Toast.LENGTH_LONG).show();
+            Intent i=new Intent(getActivity(), com.example.cho6.finalproject_guru2.Acitivity.LoginActivity.class);
+            startActivity(i);
+            //finish();
+        } catch(Exception e){
+            e.printStackTrace();
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public static String getUserIdFromUUID(String userEmail) {
+        long val = UUID.nameUUIDFromBytes(userEmail.getBytes()).getMostSignificantBits();
+        return String.valueOf(val);
     }
 }
