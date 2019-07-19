aba3ce0db94c16e20084164d944d16d674e36610
uzen.tim <itbong@amorepacific.com>
2019-07-19 15:59:11 +0900

    투표 하기 추가.

--- app/src/main/java/com/example/cho6/finalproject_guru2/Bean/ChoiceBean.java
+++ app/src/main/java/com/example/cho6/finalproject_guru2/Bean/ChoiceBean.java
@@ -1,11 +1,15 @@
 package com.example.cho6.finalproject_guru2.Bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChoiceBean implements Serializable {
 
     public boolean isSelect; //선택여부
     public String itemTitle;
+    public int totalSelCount; //선택된 갯수
+    private List<String> selectUserIdList;
 
     public ChoiceBean() {
 
@@ -16,4 +20,18 @@ public class ChoiceBean implements Serializable {
         this.itemTitle = itemTitle;
     }
 
+    public void plusTotalCount() {
+        totalSelCount += 1;
+    }
+
+    public List<String> getSelectUserIdList() {
+        if(selectUserIdList == null) {
+            selectUserIdList = new ArrayList<>();
+        }
+        return selectUserIdList;
+    }
+
+    public void setSelectUserIdList(List<String> selectUserIdList) {
+        this.selectUserIdList = selectUserIdList;
+    }
 }
